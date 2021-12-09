5c626f1935ac346ce56399ece69e5af3ab6d63c1
Kristiyan Srebrev <kpuc.cross@gmail.com>
2021-12-09 11:51:46 +0200

    about section in landing page

--- src/modules/landing/index.tsx
+++ src/modules/landing/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import { About } from 'modules/landing/components/about';
 import { Hero } from 'modules/landing/components/hero';
 
 const LandingView: React.FC = () => (
   <div className="landing-wrapper">
     <Hero />
+    <About />
   </div>
 );
 
