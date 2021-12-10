1ba668613146e203e901268fd9aa3e02153e95af
Kristiyan Srebrev <kpuc.cross@gmail.com>
2021-12-10 11:07:16 +0200

    landing page how it works section

--- src/modules/landing/index.tsx
+++ src/modules/landing/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { About } from 'modules/landing/components/about';
 import { Hero } from 'modules/landing/components/hero';
+import { HowItWorks } from 'modules/landing/components/how-it-works';
 
 const LandingView: React.FC = () => (
   <div className="landing-wrapper">
     <Hero />
     <About />
+    <HowItWorks />
   </div>
 );
 
