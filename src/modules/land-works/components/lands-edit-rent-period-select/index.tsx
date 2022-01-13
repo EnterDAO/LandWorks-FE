a412704005e43dbc4a69bf2e0cea115be6d0b4ee
Kristiyan Srebrev <kpuc.cross@gmail.com>
2022-01-13 16:49:53 +0200

    updated styles of new select components

--- src/modules/land-works/components/lands-edit-rent-period-select/index.tsx
+++ src/modules/land-works/components/lands-edit-rent-period-select/index.tsx
@@ -5,6 +5,123 @@ import { LandsEditInput } from '../lands-edit-input';
 
 import './index.scss';
 
+const styles = {
+  placeholder: () => ({
+    color: '#ffffff',
+    fontFamily: 'Poppins, sans-serif',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '16px',
+    width: 'fit-content',
+  }),
+  container: (defaultStyles: any) => ({
+    ...defaultStyles,
+    backgroundColor: '#161622',
+    boxShadow: 'transparent',
+    border: 'none',
+    borderRadius: '15px',
+    cursor: 'pointer',
+    position: 'relative',
+  }),
+  menu: (defaultStyles: any) => ({
+    ...defaultStyles,
+    position: 'absolute',
+    right: 0,
+    top: '80%',
+    background: '#161622',
+    color: '#ffffff',
+  }),
+  indicatorsContainer: () => ({
+    '& > div': {
+      padding: 0,
+      marginTop: '3px',
+      color: '#fff',
+      width: '24px',
+      height: '24px',
+    },
+  }),
+  input: (defaultStyles: any) => ({
+    ...defaultStyles,
+    appearance: 'auto',
+    caretColor: 'transparent',
+    cursor: 'pointer',
+    color: '#ffffff',
+  }),
+  singleValue: (defaultStyles: any) => ({
+    ...defaultStyles,
+    color: '#ffffff',
+  }),
+  control: (base: any, state: any) => ({
+    ...base,
+    fontFamily: 'Poppins, sans-serif',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    border: '1px solid rgba(0, 0, 0, 0.1);',
+    boxShadow: 'none',
+    '&:hover': {
+      border: '1px solid rgba(0, 0, 0, 0.1);',
+    },
+    fontSize: '16px',
+    lineHeight: '24px',
+    alignItems: 'center',
+    color: '#ffffff',
+    background: '#161622',
+    borderRadius: '12px',
+    flexGrow: '0',
+    padding: '0 8px 2px 0',
+    cursor: 'pointer',
+    minHeight: 'initial',
+    width: '100%',
+  }),
+  valueContainer: (base: any, state: any) => ({
+    ...base,
+    padding: '0 0 0 8px',
+    display: 'grid',
+  }),
+  dropdownIndicator: (base: any, state: any) => ({
+    ...base,
+    transformOrigin: '50% 50%',
+    transform: state.selectProps.menuIsOpen ? 'rotate(-180deg)' : 'rotate(0)',
+    transition: '150ms',
+    width: '24px',
+    opacity: state.isHovered ? 1 : 0.4,
+  }),
+  optionContainer: (base: any) => ({
+    ...base,
+    background: '#161622',
+    color: '#ffffff',
+  }),
+  option: (base: any, state: any) => ({
+    ...base,
+    color: state.isDisabled ? 'color: #ffffff' : '#B9B9D3',
+    fontFamily: 'Poppins, sans-serif',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    height: '30px',
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: '15px',
+    cursor: 'pointer',
+    opacity: state.isFocused ? '1' : '0.7',
+    background: state.isSelected || state.isFocused ? '#161622' : null,
+    '&:active': {
+      background: '#161622',
+    },
+    borderBottom: 'none',
+    '& label': {
+      cursor: 'pointer',
+      fontSize: '12px',
+    },
+    '&:first-of-type': {
+      borderTop: 'none',
+      borderBottom: 'none',
+    },
+    '& i::after': {
+      borderColor: 'transparent !important',
+    },
+  }),
+};
+
 type Item = {
   label: string;
   value: number | string;
@@ -36,6 +153,7 @@ export const LandsEditPeriodDropdown: React.FC<IProps> = ({
         value={initialValuе}
         onChange={onChange}
         className="lands-period-drop"
+        styles={styles}
       />
     </div>
   );
