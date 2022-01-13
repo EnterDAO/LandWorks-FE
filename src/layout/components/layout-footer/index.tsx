6e5e6ecbdf826484c8e34e8b869269e85ddc3144
Vladislav Ivanov <ivanov.vladdy@gmail.com>
2022-01-13 09:27:14 +0200

    Sharded Minds link

--- src/layout/components/layout-footer/index.tsx
+++ src/layout/components/layout-footer/index.tsx
@@ -77,10 +77,10 @@ const LayoutFooter: React.FC = () => {
                       NFT Drops
                     </Link>
                   </Col>
-                  <Col xs={13} sm={13} md={13} lg={24} xl={24} className={s.comingSoon}>
-                    <Link to="/" className={`${s.linkAnchor}`}>
+                  <Col xs={13} sm={13} md={13} lg={24} xl={24}>
+                    <ExternalLink href="https://sharded-minds.enterdao.xyz" className={(s.linkAnchor, s.linkLandWorks)}>
                       Sharded Minds
-                    </Link>
+                    </ExternalLink>
                   </Col>
                 </Row>
               </Col>
