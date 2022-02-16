6b111d1d43b215430a8f0c16efbc2d19952b5ed4
Anzette <anzette@linumlabs.com>
2022-02-16 21:07:55 +0200

    refactor subheader for explore page

---
+++ src/modules/land-works/components/lands-explore-subheader/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { ReactComponent as AddIcon } from '../../../../resources/svg/add.svg';
+
+import './index.scss';
+
+interface Props {
+  totalLands: number;
+  hasMetamaskConnected: boolean;
+}
+
+export const LandsSubheader: React.FC<Props> = ({ totalLands, hasMetamaskConnected }) => {
+  const history = useHistory();
+
+  return (
+    <div className="lands-header">
+      <h1>
+        Explore Properties <span>Total listed {totalLands}</span>
+      </h1>
+      {hasMetamaskConnected && (
+        <div className="addTokenWrapper">
+          <button type="button" className={`button-primary list-new-property`} onClick={() => history.push('/list')}>
+            <AddIcon className={`add-icon`} />
+            List New Property
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
