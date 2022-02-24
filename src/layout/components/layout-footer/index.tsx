091d03a4b22a210304aec47d0f8dc14d3aa03ba6
Rocco Russo <xdemocle@gmail.com>
2022-02-24 13:49:52 +0200

    Feature/rr/179 (#224)
    * [179] implementing footer design globally and modular for explore-view
    
    * [179] implementing footer design globally and modular for explore-view

--- src/layout/components/layout-footer/index.tsx
+++ src/layout/components/layout-footer/index.tsx
@@ -1,114 +1,65 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { Col, Row } from 'antd';
-import cn from 'classnames';
 
 import ExternalLink from 'components/custom/externalLink';
 import Icon from 'components/custom/icon';
 
-import s from './s.module.scss';
+import styles from './layout-footer.module.scss';
 
-const LayoutFooter: React.FC = () => {
+interface Props {
+  isWrapped?: boolean;
+}
+
+const LayoutFooter: React.FC<Props> = ({ isWrapped = true }) => {
   const getYear = () => {
     return new Date().getFullYear();
   };
 
   return (
-    <footer className={s.footer}>
-      <div className="container-limit">
-        <Row
-          className={s.footerContent}
-          gutter={[
-            { xs: 8, sm: 16, md: 24, lg: 32 },
-            { xs: 8, sm: 16, md: 24, lg: 32 },
-          ]}
-        >
-          <Col xs={24} sm={24} md={24} lg={17} xl={17} className={s.info}>
-            <Row>
-              <Col xs={24} sm={24} md={24} lg={14} xl={12}>
-                <Row align={'middle'} gutter={20}>
-                  <Col xs={6} sm={3} md={4} lg={4} xl={4} style={{ textAlign: 'center' }}>
-                    <Icon name="png/LandWorksLogo" width="56" height="56" />
-                  </Col>
-                  <Col xs={18} sm={21} md={20} lg={18} xl={18}>
-                    <p className={s.text}>
-                      Rent or Lend metaverse assets through community-owned Marketplace, governed by the $ENTR token.
-                    </p>
-                  </Col>
-                </Row>
-              </Col>
-              <Col
-                xs={{ span: 24 }}
-                sm={{ span: 24 }}
-                md={{ span: 24 }}
-                lg={{ span: 10 }}
-                xl={{ span: 10 }}
-                style={{ textAlign: 'end', display: 'flex', alignItems: 'center' }}
-              >
-                <span className={(s.text, s.subline)}>
-                  a project by <strong>EnterDAO</strong>{' '}
-                </span>
-                <Icon style={{ marginLeft: '6px' }} name="png/enterdao" width="56" height="56" />
-              </Col>
-            </Row>
-          </Col>
-          <Col xs={24} sm={24} md={24} lg={7} xl={7} className={s.nav}>
-            <div className={s.linksRow}>
-              <div className={s.linksWrapper}>
-                <Row justify="center">
-                  <Col xs={13} sm={13} md={13} lg={24} xl={24}>
-                    <Link to="/" className={s.linkTitle}>
-                      Products
-                    </Link>
-                  </Col>
-                  <Col xs={13} sm={13} md={13} lg={24} xl={24}>
-                    <Link to="/" className={(s.linkAnchor, s.linkLandWorks)}>
-                      LandWorks
-                    </Link>
-                  </Col>
-                  <Col xs={13} sm={13} md={13} lg={24} xl={24}>
-                    <ExternalLink href="https://metaportal.gg/" className={s.linkAnchor}>
-                      MetaPortal
-                    </ExternalLink>
-                  </Col>
-                </Row>
-              </div>
-              <div className={s.linksWrapper}>
-                <Row justify="center">
-                  <Col xs={13} sm={13} md={13} lg={24} xl={24}>
-                    <Link to="/" className={s.linkTitle}>
-                      NFT Drops
-                    </Link>
-                  </Col>
-                  <Col xs={13} sm={13} md={13} lg={24} xl={24}>
-                    <ExternalLink href="https://sharded-minds.enterdao.xyz" className={s.linkAnchor}>
-                      Sharded Minds
-                    </ExternalLink>
-                  </Col>
-                </Row>
-              </div>
-            </div>
-          </Col>
-        </Row>
-        <div className={cn(s.row, s.copyrightsBlock)}>
-          <div className={s.copyrightLink}>landworks.xyz © {getYear()}. Open-sourced.</div>
-          <div className={s.sLinksWrap}>
-            {/* //TODO:: Update the icon with Github */}
-            <ExternalLink href="https://github.com/EnterDAO" className={s.sLink}>
-              <Icon name="png/Github" width="20" height="20" />
-            </ExternalLink>
-            <ExternalLink href="https://twitter.com/EnterDao" className={s.sLink}>
-              <Icon name="twitter" width="20" height="20" />
-            </ExternalLink>
-            <ExternalLink href="https://discord.gg/7QJvEctG2G" className={s.sLink}>
-              <Icon name="discord" width="20" height="20" />
-            </ExternalLink>
-            <ExternalLink href="https://www.coingecko.com/en/coins/enterdao" className={s.sLink}>
-              <Icon name="coingecko" width="20" height="20" />
-            </ExternalLink>
-            <ExternalLink href="https://medium.com/enterdao" className={s.sLink}>
-              <Icon name="medium" width="20" height="20" />
-            </ExternalLink>
-          </div>
+    <footer className={`${styles.root} ${isWrapped ? styles['root--wrapped'] : ''}`}>
+      <div className={styles.container}>
+        <div className={styles['social-media-container']}>
+          <ExternalLink href="https://github.com/EnterDAO" className={styles['social-media-container--link']}>
+            <Icon name="png/Github" width="20" height="20" />
+          </ExternalLink>
+          <ExternalLink href="https://twitter.com/EnterDao" className={styles['social-media-container--link']}>
+            <Icon name="twitter" width="20" height="20" />
+          </ExternalLink>
+          <ExternalLink href="https://discord.gg/7QJvEctG2G" className={styles['social-media-container--link']}>
+            <Icon name="discord" width="20" height="20" />
+          </ExternalLink>
+          <ExternalLink
+            href="https://www.coingecko.com/en/coins/enterdao"
+            className={styles['social-media-container--link']}
+          >
+            <Icon name="coingecko" width="20" height="20" />
+          </ExternalLink>
+          <ExternalLink href="https://medium.com/enterdao" className={styles['social-media-container--link']}>
+            <Icon name="medium" width="20" height="20" />
+          </ExternalLink>
+        </div>
+
+        <div className={styles['links-container']}>
+          <ExternalLink href="https://enterdao.xyz/" className={styles.linkAnchor}>
+            EnterDAO
+          </ExternalLink>
+
+          <Link to="/" className={styles.linkAnchor}>
+            LandWorks
+          </Link>
+
+          <ExternalLink href="https://metaportal.gg/" className={styles.linkAnchor}>
+            MetaPortal
+          </ExternalLink>
+
+          <ExternalLink href="https://docs.landworks.xyz/" className={styles.linkAnchor}>
+            Documentation
+          </ExternalLink>
+        </div>
+
+        <div className={styles.bottom}>
+          <Icon name="png/LandWorksLogo" width="39" height="37" />
+          <div className={styles.copyrightLink}>landworks.xyz © {getYear()}. Open-sourced.</div>
         </div>
       </div>
     </footer>
