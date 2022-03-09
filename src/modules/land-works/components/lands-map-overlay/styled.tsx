173f15733b1e7bf138577ddf3db665e1bbfb4b19
Rocco Russo <xdemocle@gmail.com>
2022-03-09 18:41:09 +0200

    [174] redesign property details (#260)
    * [174] add fixes
    
    * [174] fixes
    
    * [174] fixes
    
    * [174] refactoring chips
    
    * [174] refactoring chips
    
    * [174] latest refactoring

---
+++ src/modules/land-works/components/lands-map-overlay/styled.tsx
@@ -0,0 +1,34 @@
+import { Chip, ChipProps, Typography, TypographyProps, styled } from '@mui/material';
+
+export const RootStyled = styled('div')(() => ({
+  position: 'absolute',
+  bottom: '4%',
+  right: '4%',
+  display: 'block',
+  backgroundColor: 'var(--theme-body-color)',
+  padding: '16px',
+  borderRadius: '20px',
+  maxWidth: '17rem',
+}));
+
+export const TypographyStyled = styled(Typography)<TypographyProps>(() => ({
+  fontSize: '14px',
+  lineHeight: '28px',
+  color: '#fff',
+}));
+
+export const ChipStyled = styled(Chip)<ChipProps>(() => ({
+  display: 'inline-block',
+  margin: '0 0.25rem 0.3rem 0',
+  float: 'left',
+  backgroundColor: 'var(--theme-modal-color)',
+  color: 'var(--theme-subtle-color)',
+  fontSize: '14px',
+  fontWeight: 700,
+  lineHeight: '28px',
+  height: '28px',
+  borderRadius: '5px',
+  border: 0,
+  whiteSpace: 'normal',
+  textAlign: 'center',
+}));
