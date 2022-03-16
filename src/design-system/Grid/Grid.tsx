dd82a9cc5be336104f44a370a0045bf790c5b942
Rocco Russo <xdemocle@gmail.com>
2022-03-16 08:35:23 +0200

    [262] implementing a custom Grid with extra breakpoint. (#291)
    * [262] implementing a custom Grid with extra breakpoint.
    
    * [262] fixing warning eslint

---
+++ src/design-system/Grid/Grid.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+import { Grid as MuiGrid, GridProps as MuiGridProps, styled } from '@mui/material';
+
+type GridProps = MuiGridProps & {
+  xxl?: number;
+};
+
+export const MuiGridStyled = styled(MuiGrid)(({ theme }) => ({
+  '&.MuiGrid-grid-xxl-4': {
+    [theme.breakpoints.up(1600)]: {
+      maxWidth: '33%',
+    },
+  },
+}));
+
+const Grid: FC<GridProps> = ({ children, xxl, ...other }) => {
+  return (
+    <MuiGridStyled className={`MuiGrid-grid-xxl-${xxl}`} {...other}>
+      {children}
+    </MuiGridStyled>
+  );
+};
+
+export default Grid;
