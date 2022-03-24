a80f75f9e789a20700c13f589f47df0d1670c754
anzette <anzette@linumlabs.com>
2022-03-24 13:39:43 +0200

    create larger skeleton

--- src/modules/land-works/components/land-single-card-loader/index.tsx
+++ src/modules/land-works/components/land-single-card-loader/index.tsx
@@ -5,20 +5,33 @@ import Skeleton from 'react-loading-skeleton';
 
 import { Box } from 'design-system';
 
-import { GridStyled } from './styled';
+import { GridStyled, GridStyledInnerContainer } from './styled';
 
 const highlightColor = '#68686a';
 const baseColor = '#c1c1cf';
 
 const SingleLandCardSkeleton: FC = () => {
   return (
-    <GridStyled mt={0} mr={1} xs={5.5} minHeight={350} container flexDirection="column" spacing={2}>
-      <Box height={'80%'}>
-        <Skeleton width={'100%'} height={'100%'} baseColor={baseColor} highlightColor={highlightColor} duration={3} />
-      </Box>
-      <Box height={'10%'} mt={3}>
-        <Skeleton width={'100%'} height={'100%'} baseColor={baseColor} highlightColor={highlightColor} duration={3} />
-      </Box>
+    <GridStyled mt={0} xs={12} minHeight={350} container columnSpacing={2}>
+      <GridStyledInnerContainer mr={3} xs={5.8} flexDirection="column">
+        <Box height={'80%'}>
+          <Skeleton width={'100%'} height={'100%'} baseColor={baseColor} highlightColor={highlightColor} duration={3} />
+        </Box>
+        <Box height={'10%'} mt={3}>
+          <Skeleton width={'100%'} height={'100%'} baseColor={baseColor} highlightColor={highlightColor} duration={3} />
+        </Box>
+      </GridStyledInnerContainer>
+      <GridStyledInnerContainer xs={5.8} flexDirection="column">
+        <Box height={'20%'}>
+          <Skeleton width={'100%'} height={'100%'} baseColor={baseColor} highlightColor={highlightColor} duration={3} />
+        </Box>
+        <Box height={'20%'} mt={3}>
+          <Skeleton width={'100%'} height={'100%'} baseColor={baseColor} highlightColor={highlightColor} duration={3} />
+        </Box>
+        <Box height={'45%'} mt={3}>
+          <Skeleton width={'100%'} height={'100%'} baseColor={baseColor} highlightColor={highlightColor} duration={3} />
+        </Box>
+      </GridStyledInnerContainer>
     </GridStyled>
   );
 };
--- src/modules/land-works/components/land-single-card-loader/styled.tsx
+++ src/modules/land-works/components/land-single-card-loader/styled.tsx
@@ -3,12 +3,19 @@ import { styled } from '@mui/system';
 
 export const GridStyled = styled(Grid)(() => ({
   display: 'flex',
-  flexDirection: 'column',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  background: 'none',
+  border: '1px solid rgba(0, 0, 0, 0.1)',
+  boxSizing: 'border-box',
+  transition: '0.3s ease-in-out',
+}));
+
+export const GridStyledInnerContainer = styled(Grid)(() => ({
   background: 'var(--theme-modal-color)',
   border: '1px solid rgba(0, 0, 0, 0.1)',
+  boxShadow: '0px 10px 36px rgba(136, 120, 172, 0.14)',
   boxSizing: 'border-box',
   borderRadius: '20px',
   padding: '25px',
-  transition: '0.3s ease-in-out',
-  boxShadow: '0px 10px 36px rgba(136, 120, 172, 0.14)',
 }));
