a5317a9df468e5c2d86e61f0fb0bfafd30682600
mishaadzh <work.adzhoiev@gmail.com>
2022-03-30 11:05:15 +0300

    400 / tooltip styles

--- src/design-system/Tooltip/tooltip-styles.ts
+++ src/design-system/Tooltip/tooltip-styles.ts
@@ -5,11 +5,13 @@ import { THEME_COLORS } from '../../themes/theme-constants';
 
 const StyledTooltip = withStyles({
   tooltip: {
-    background: THEME_COLORS.light,
-    color: THEME_COLORS.grey01,
+    background: 'linear-gradient(0deg, rgba(248, 248, 255, 0.1), rgba(248, 248, 255, 0.1)), #27273A;',
+    color: THEME_COLORS.grey03,
     padding: '13px 24px',
     maxWidth: '285px',
     borderRadius: '10px',
+    border: '3px solid ' + THEME_COLORS.light,
+    textAlign: 'center',
     [`&.${tooltipClasses.tooltipArrow}`]: {
       padding: '15px',
       width: '285px',
