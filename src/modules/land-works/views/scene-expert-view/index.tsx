31a022fc8c778573a048a3d98cebdca514e9a910
anzette <anzette@linumlabs.com>
2022-03-31 13:36:51 +0200

    create builder card skeleton

--- src/modules/land-works/views/scene-expert-view/index.tsx
+++ src/modules/land-works/views/scene-expert-view/index.tsx
@@ -4,6 +4,7 @@ import { useNotion } from 'api/notion/client';
 import { Grid } from 'design-system';
 import SceneExpertHeading from 'modules/land-works/components/land-works-scene-experts-heading';
 import SceneExpertCard from 'modules/land-works/components/scene-expert-card';
+import CardLoaderSkeleton from 'modules/land-works/components/scene-expert-card/card-loader-skeleton';
 
 import { transformSceneProvider } from 'modules/land-works/utils';
 
@@ -12,6 +13,7 @@ import { NotionResultForCard } from 'modules/land-works/components/scene-expert-
 const SceneExpertView: FC = () => {
   const { getSceneProviders } = useNotion();
   const [sceneBuilders, setSceneBuilders] = useState<NotionResultForCard[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
@@ -19,20 +21,31 @@ const SceneExpertView: FC = () => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const data = sceneProv.results.map((i: any) => transformSceneProvider(i));
       setSceneBuilders(data);
+      setLoading(false);
     })();
   }, []);
 
   return (
     <Grid className="content-container">
       <SceneExpertHeading />
-      <Grid container spacing={4} rowSpacing={4} columnSpacing={4}>
-        {sceneBuilders.length &&
-          sceneBuilders.map((builder) => (
-            <Grid item xs={12} sm={6} md={6} lg={4} xl={3} key={builder.builderName}>
-              <SceneExpertCard builder={builder} />
+      {loading ? (
+        <Grid container spacing={4} rowSpacing={4} columnSpacing={4}>
+          {[1, 2, 3, 4].map((i) => (
+            <Grid item xs={12} sm={6} md={6} lg={4} xl={3} key={i}>
+              <CardLoaderSkeleton key={i} />
             </Grid>
           ))}
-      </Grid>
+        </Grid>
+      ) : (
+        <Grid container spacing={4} rowSpacing={4} columnSpacing={4}>
+          {sceneBuilders.length &&
+            sceneBuilders.map((builder) => (
+              <Grid item xs={12} sm={6} md={6} lg={4} xl={3} key={builder.builderName}>
+                <SceneExpertCard builder={builder} />
+              </Grid>
+            ))}
+        </Grid>
+      )}
     </Grid>
   );
 };
