8cb02c5744e16d123d2e9f7b0a975156e019e014
anzette <anzette@linumlabs.com>
2022-04-04 09:51:23 +0200

    create tabs and filter functionality

--- src/modules/land-works/views/scene-expert-view/index.tsx
+++ src/modules/land-works/views/scene-expert-view/index.tsx
@@ -1,10 +1,17 @@
 import { FC, useEffect, useState } from 'react';
+import {
+  SCENE_EXPERT_TAB_STATE_ALL,
+  SCENE_EXPERT_TAB_STATE_INDIVIDUAL,
+  SCENE_EXPERT_TAB_STATE_STUDIO,
+} from 'constants/modules';
+import TabContext from '@mui/lab/TabContext';
 
 import { useNotion } from 'api/notion/client';
 import { Grid } from 'design-system';
 import SceneExpertHeading from 'modules/land-works/components/land-works-scene-experts-heading';
 import SceneExpertCard from 'modules/land-works/components/scene-expert-card';
 import CardLoaderSkeleton from 'modules/land-works/components/scene-expert-card/card-loader-skeleton';
+import SceneExpertTabs from 'modules/land-works/components/scene-expert-tabs';
 
 import { transformSceneProvider } from 'modules/land-works/utils';
 
@@ -13,7 +20,9 @@ import { NotionResultForCard } from 'modules/land-works/components/scene-expert-
 const SceneExpertView: FC = () => {
   const { getSceneProviders } = useNotion();
   const [sceneBuilders, setSceneBuilders] = useState<NotionResultForCard[]>([]);
+  const [filteredData, setFilteredData] = useState(sceneBuilders);
   const [loading, setLoading] = useState(true);
+  const [tab, setTab] = useState(SCENE_EXPERT_TAB_STATE_ALL);
 
   useEffect(() => {
     (async () => {
@@ -23,29 +32,47 @@ const SceneExpertView: FC = () => {
       setSceneBuilders(data);
       setLoading(false);
     })();
-  }, []);
+  }, [tab]);
+
+  const individualBuilders = sceneBuilders?.filter((i) => i.builderType === 'Individual');
+  const studioBuilders = sceneBuilders?.filter((i) => i.builderType === 'Studio');
+
+  console.log({ individualBuilders, studioBuilders }, studioBuilders);
+
+  useEffect(() => {
+    if (tab === SCENE_EXPERT_TAB_STATE_INDIVIDUAL && individualBuilders !== undefined) {
+      setFilteredData(individualBuilders);
+    } else if (tab === SCENE_EXPERT_TAB_STATE_STUDIO && studioBuilders !== undefined) {
+      setFilteredData(studioBuilders);
+    } else {
+      setFilteredData(sceneBuilders);
+    }
+  }, [tab, sceneBuilders]);
 
   return (
     <Grid className="content-container">
       <SceneExpertHeading />
-      {loading ? (
-        <Grid container spacing={4} rowSpacing={4} columnSpacing={4}>
-          {[1, 2, 3, 4].map((i) => (
-            <Grid item xs={12} sm={6} md={6} lg={4} xl={3} key={i}>
-              <CardLoaderSkeleton key={i} />
-            </Grid>
-          ))}
-        </Grid>
-      ) : (
-        <Grid container spacing={4} rowSpacing={4} columnSpacing={4}>
-          {sceneBuilders.length &&
-            sceneBuilders.map((builder) => (
-              <Grid item xs={12} sm={6} md={6} lg={4} xl={3} key={builder.builderName}>
-                <SceneExpertCard builder={builder} />
+      <TabContext value={tab}>
+        <SceneExpertTabs setTab={setTab} />
+        {loading ? (
+          <Grid container spacing={4} rowSpacing={4} columnSpacing={4}>
+            {[1, 2, 3, 4].map((i) => (
+              <Grid item xs={12} sm={6} md={6} lg={4} xl={3} key={i}>
+                <CardLoaderSkeleton key={i} />
               </Grid>
             ))}
-        </Grid>
-      )}
+          </Grid>
+        ) : (
+          <Grid container spacing={4} rowSpacing={4} columnSpacing={4}>
+            {sceneBuilders.length &&
+              filteredData.map((builder) => (
+                <Grid item xs={12} sm={6} md={6} lg={4} xl={3} key={builder.builderName}>
+                  <SceneExpertCard builder={builder} />
+                </Grid>
+              ))}
+          </Grid>
+        )}
+      </TabContext>
     </Grid>
   );
 };
