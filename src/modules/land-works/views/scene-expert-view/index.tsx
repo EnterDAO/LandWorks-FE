f54eb24df3abb3519b1f7fa519c42e84b3b6fa22
Anzette Klopper <50806665+zetske@users.noreply.github.com>
2022-04-07 13:24:13 +0200

    307 (scene expert individual page) (#438)
    * create expert profile page and main profile card
    
    * create types for function to fetch scene builder
    
    * add linkedin icon
    
    * clean up
    
    * add details card for expert
    
    * add dynamic route to expert page
    
    * clean up
    
    * add skeleton to single expert view page
    
    * clean up
    
    * remove unused design components and adjust container layout
    
    * clean up styling
    
    * clean up comments
    
    * create expert porfolio section
    
    * clean up styling
    
    * add header
    
    * improve responsive size for containers
    
    * set hover effect for expert card
    
    * dynamically render expert and portfolio according to route
    
    * use params to render expert details and clean up
    
    * improve positioning for expert picture
    
    * clean up
    
    * solve svg issue

--- src/modules/land-works/views/scene-expert-view/index.tsx
+++ src/modules/land-works/views/scene-expert-view/index.tsx
@@ -13,7 +13,7 @@ import SceneExpertCard from 'modules/land-works/components/scene-expert-card';
 import CardLoaderSkeleton from 'modules/land-works/components/scene-expert-card/card-loader-skeleton';
 import SceneExpertTabs from 'modules/land-works/components/scene-expert-tabs';
 
-import { transformSceneProvider } from 'modules/land-works/utils';
+import { transformSceneProviderForCard } from 'modules/land-works/utils';
 
 import { NotionResultForCard } from 'modules/land-works/components/scene-expert-card/types';
 
@@ -28,7 +28,7 @@ const SceneExpertView: FC = () => {
     (async () => {
       const sceneProv = await getSceneProviders();
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const data = sceneProv.results.map((i: any) => transformSceneProvider(i));
+      const data = sceneProv.results.map((i: any) => transformSceneProviderForCard(i));
       setSceneBuilders(data);
       setLoading(false);
     })();
---
+++ src/modules/land-works/views/single-scene-expert-view/index.tsx
@@ -0,0 +1,84 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { FC, useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
+import { useNotion } from 'api/notion/client';
+import { Grid } from 'design-system';
+import { ArrowRightIcon, BackIcon } from 'design-system/icons';
+import SceneExpertProfile from 'modules/land-works/components/scene-expert-profile';
+import ProfileLoaderSkeleton from 'modules/land-works/components/scene-expert-profile/card-loader-skeleton';
+import SceneExpertDetails from 'modules/land-works/components/scene-expert-profile/scene-expert-details';
+import SceneExpertPortfolio from 'modules/land-works/components/scene-expert-profile/scene-expert-portfolio';
+
+import { BreadCrumbs, Separator } from '../scene-expert-form-view/styled';
+
+import { transformSceneProviderForProfile } from 'modules/land-works/utils';
+
+import { NotionResultForProfile } from 'modules/land-works/components/scene-expert-card/types';
+
+interface SingleExpertViewParams {
+  expertName: string;
+}
+
+const SingleExpertView: FC = () => {
+  const { getSceneProviders } = useNotion();
+  const [loading, setLoading] = useState(true);
+  const [selectedExpert, setSelectedExpert] = useState<NotionResultForProfile>();
+  const expertFromParam = useParams() as SingleExpertViewParams;
+  const { expertName } = expertFromParam;
+
+  useEffect(() => {
+    (async () => {
+      const sceneProv = await getSceneProviders();
+      const data = sceneProv.results.map((i: any) => transformSceneProviderForProfile(i));
+      const filterDataByExpertName = data?.find((e) => e.builderName === expertName);
+      setSelectedExpert(filterDataByExpertName!);
+      setLoading(false);
+    })();
+  }, [expertFromParam]);
+
+  const hasPortfolio = selectedExpert?.portfolio[0] !== undefined;
+
+  return (
+    <Grid className="content-container">
+      <BreadCrumbs>
+        <Link className="button-back" to={'/scene-expert'}>
+          <div className="button-icon">
+            <BackIcon style={{ width: '20px' }} />
+          </div>
+          <span>Back to {'Scene Experts'}</span>
+        </Link>
+        <Separator />
+        <Link className="button-explore" to={'/scene-expert'}>
+          <span>{'Experts'}</span>
+        </Link>
+        <ArrowRightIcon style={{ width: '20px' }} />
+        <span>{selectedExpert?.builderName}</span>
+      </BreadCrumbs>
+      {loading ? (
+        <ProfileLoaderSkeleton />
+      ) : (
+        <>
+          <Grid mt="28px" container spacing={2} rowSpacing={4} columnSpacing={4}>
+            <Grid item xs={12} sm={12} md={12} lg={8}>
+              <SceneExpertProfile builder={selectedExpert!} />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={4}>
+              <SceneExpertDetails builder={selectedExpert!} />
+            </Grid>
+          </Grid>
+          {selectedExpert && hasPortfolio && (
+            <Grid mt="28px" container spacing={2} rowSpacing={4} columnSpacing={4}>
+              <Grid item xs={8}>
+                <SceneExpertPortfolio portfolio={selectedExpert?.portfolio} />
+              </Grid>
+            </Grid>
+          )}
+        </>
+      )}
+    </Grid>
+  );
+};
+
+export default SingleExpertView;
