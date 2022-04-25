0365b6dfa692fb34b593600c82815f2f26f3c9e8
xdemocle <xdemocle@gmail.com>
2022-04-25 11:12:40 +0200

    removing antd/button and not used components and views

--- src/modules/land-works/index.tsx
+++ src/modules/land-works/index.tsx
@@ -9,13 +9,10 @@ import SceneBuilderFormView from './views/scene-builder-form-view';
 import SceneBuilderView from './views/scene-builder-view';
 import SingleBuilderView from './views/single-scene-builder-view';
 
-const RentingView = lazy(() => import('./views/my-renting-view'));
-const LendingView = lazy(() => import('./views/my-lending-view'));
 const LandsView = lazy(() => import('./views/lands-view'));
 const ExploreView = lazy(() => import('./views/explore-view'));
 const MyPropertiesView = lazy(() => import('./views/my-properties-view'));
 const SingleLand = lazy(() => import('./views/single-land-view'));
-const ListProperty = lazy(() => import('./views/list-property-view'));
 const EditProperty = lazy(() => import('./views/edit-property-view'));
 const LandingView = lazy(() => import('modules/landing'));
 
@@ -47,21 +44,6 @@ const LandworksView: React.FC = () => {
       <Route path="/scene-builder" exact component={SceneBuilderView} />
       <Route path="/scene-builder/:builderName" exact component={SingleBuilderView} />
       <Route path="/join-builders" exact component={SceneBuilderFormView} />
-      <Route path="/lending" exact component={LendingView} />
-      <ProtectedRoute
-        isAuthenticated={false}
-        authenticationPath="/my-properties"
-        path="/renting"
-        exact
-        component={RentingView}
-      />
-      <ProtectedRoute
-        isAuthenticated={false}
-        authenticationPath="/explore"
-        path="/list"
-        exact
-        component={ListProperty}
-      />
       <ProtectedRoute
         isAuthenticated={false}
         authenticationPath="/explore"
