a9bdcb6ed5130f4f9236c78ec368654a723053c7
anzette <anzette@linumlabs.com>
2022-04-28 13:50:06 +0200

    improve property container layout

--- src/modules/land-works/components/land-parcel-properties/index.tsx
+++ src/modules/land-works/components/land-parcel-properties/index.tsx
@@ -1,90 +1,93 @@
-import { Grid } from 'design-system';
+import { useEffect, useState } from 'react';
+
 import { AssetAttributes } from 'modules/land-works/api';
 
-import { GridStyled, GridStyledInnerContainer, TypographyStyled } from './styled';
+import ParcelPropertiesLoader from '../land-parcel-properties-loader';
+import { GridFlexed, GridItem, GridStyled, GridStyledInnerContainer, TypographyStyled } from './styled';
 
 interface Props {
   attributes: AssetAttributes;
   metaverseAssetId: string;
 }
 
-const SingleViewParcelProprties: React.FC<Props> = ({ attributes, metaverseAssetId }) => {
+const SingleViewParcelProperties: React.FC<Props> = ({ attributes, metaverseAssetId }) => {
+  const [loading, setLoading] = useState(true);
   const atr = attributes;
 
-  const locationDetails = [
-    { title: '', value: atr.suburb },
-    { title: '', value: metaverseAssetId },
-    { title: '', value: atr.suburb },
-  ];
-
-  const parcelSize = [
-    { title: '', value: atr?.area },
-    { title: '', value: '10m x 10m' },
-    { title: '', value: atr?.height },
-    { title: '', value: atr?.elevation },
-  ];
-
-  const additions = [
-    { title: '', value: atr.has_basement },
-    { title: '', value: atr.waterfront },
-    { title: '', value: 10 },
-  ];
-
-  console.log({ atr });
+  useEffect(() => {
+    if (atr?.suburb) {
+      setLoading(false);
+    }
+  }, [atr]);
 
   return (
     <GridStyled minHeight={288} container>
-      <GridStyledInnerContainer item xs={5} flexDirection="column" alignItems={'left'}>
-        <TypographyStyled variant="h1">Location Details</TypographyStyled>
-        <Grid display="flex" flexDirection="row">
-          <Grid item xs={4.5} mr={5}>
-            <TypographyStyled variant="h2">Address</TypographyStyled>
-            <TypographyStyled variant="h2">Token</TypographyStyled>
-            <TypographyStyled variant="h2">Neighborhood</TypographyStyled>
-          </Grid>
-          <Grid item xs={7}>
-            <TypographyStyled variant="h3">
-              {atr?.island}, {atr?.suburb}
-            </TypographyStyled>
-            <TypographyStyled variant="h3">{metaverseAssetId}</TypographyStyled>
-            <TypographyStyled variant="h3">{atr?.suburb}</TypographyStyled>
-          </Grid>
-        </Grid>
-      </GridStyledInnerContainer>
-      <GridStyledInnerContainer item xs={3.2} flexDirection="column">
-        <TypographyStyled variant="h1">Parcel Size</TypographyStyled>
-        <Grid display="flex" flexDirection="row">
-          <Grid item xs={4.5} mr={5}>
-            <TypographyStyled variant="h2">Area</TypographyStyled>
-            <TypographyStyled variant="h2">Size</TypographyStyled>
-            <TypographyStyled variant="h2">Build height</TypographyStyled>
-            <TypographyStyled variant="h2">Elevation</TypographyStyled>
-          </Grid>
-          <Grid item xs={7}>
-            <TypographyStyled variant="h3">{atr?.area}m^2</TypographyStyled>
-            <TypographyStyled variant="h3">Unknown?</TypographyStyled>
-            <TypographyStyled variant="h3">{atr?.height} metres</TypographyStyled>
-            <TypographyStyled variant="h3">{atr?.elevation}</TypographyStyled>
-          </Grid>
-        </Grid>
-      </GridStyledInnerContainer>
-      <GridStyledInnerContainer item xs={3.2} flexDirection="column">
-        <TypographyStyled variant="h1">Additions</TypographyStyled>
-        <Grid display="flex" flexDirection="row">
-          <Grid item xs={4.5} mr={5}>
-            <TypographyStyled variant="h2">Has Basement</TypographyStyled>
-            <TypographyStyled variant="h2">Is Waterfront</TypographyStyled>
-            <TypographyStyled variant="h2">Volume</TypographyStyled>
-          </Grid>
-          <Grid item xs={7}>
-            <TypographyStyled variant="h3">{atr?.has_basement ? 'Yes' : 'No'}</TypographyStyled>
-            <TypographyStyled variant="h3">{atr?.waterfront ? 'Yes' : 'No'}</TypographyStyled>
-            <TypographyStyled variant="h3">Neighborhood</TypographyStyled>
-          </Grid>
-        </Grid>
-      </GridStyledInnerContainer>
+      <TypographyStyled variant="h1">Parcel Properties</TypographyStyled>
+      {loading ? (
+        <GridFlexed>
+          <ParcelPropertiesLoader />
+        </GridFlexed>
+      ) : (
+        <>
+          <GridFlexed>
+            <GridStyledInnerContainer item xs={5} flexDirection="column" alignItems={'left'}>
+              <TypographyStyled variant="h2">Location Details</TypographyStyled>
+              <GridItem item display="flex" flexDirection="row" xs={12}>
+                <TypographyStyled variant="h3">Address</TypographyStyled>
+                <TypographyStyled variant="h4">
+                  {atr?.island}, {atr?.suburb}
+                </TypographyStyled>
+              </GridItem>
+              <GridItem item display="flex" flexDirection="row" xs={12}>
+                <TypographyStyled variant="h3">Token</TypographyStyled>
+                <TypographyStyled variant="h4">{metaverseAssetId}</TypographyStyled>
+              </GridItem>
+              <GridItem item display="flex" flexDirection="row" xs={12}>
+                <TypographyStyled variant="h3">Neighborhood</TypographyStyled>
+                <TypographyStyled variant="h4">{atr?.suburb}</TypographyStyled>
+              </GridItem>
+            </GridStyledInnerContainer>
+
+            <GridStyledInnerContainer item xs={3.4} flexDirection="column" alignItems={'left'}>
+              <TypographyStyled variant="h2">Parcel Size</TypographyStyled>
+              <GridItem item display="flex" flexDirection="row" xs={12}>
+                <TypographyStyled variant="h3">Area</TypographyStyled>
+                <TypographyStyled variant="h4">{atr?.area}m²</TypographyStyled>
+              </GridItem>
+              <GridItem item display="flex" flexDirection="row" xs={12}>
+                <TypographyStyled variant="h3">Size</TypographyStyled>
+                <TypographyStyled variant="h4">Unknown?</TypographyStyled>
+              </GridItem>
+              <GridItem item display="flex" flexDirection="row" xs={12}>
+                <TypographyStyled variant="h3">Build height</TypographyStyled>
+                <TypographyStyled variant="h4">{atr?.height} metres</TypographyStyled>
+              </GridItem>
+              <GridItem item display="flex" flexDirection="row" xs={12}>
+                <TypographyStyled variant="h3">Elevation</TypographyStyled>
+                <TypographyStyled variant="h4">{atr?.elevation}</TypographyStyled>
+              </GridItem>
+            </GridStyledInnerContainer>
+
+            <GridStyledInnerContainer item xs={3.4} flexDirection="column" alignItems={'left'}>
+              <TypographyStyled variant="h2">Additions</TypographyStyled>
+              <GridItem item display="flex" flexDirection="row" xs={12}>
+                <TypographyStyled variant="h3">Has Basement</TypographyStyled>
+                <TypographyStyled variant="h4">{atr?.has_basement ? 'Yes' : 'No'}</TypographyStyled>
+              </GridItem>
+              <GridItem item display="flex" flexDirection="row" xs={12}>
+                <TypographyStyled variant="h3">Is Waterfront</TypographyStyled>
+                <TypographyStyled variant="h4">{atr?.waterfront ? 'Yes' : 'No'}</TypographyStyled>
+              </GridItem>
+              <GridItem item display="flex" flexDirection="row" xs={12}>
+                <TypographyStyled variant="h3">Volume</TypographyStyled>
+                <TypographyStyled variant="h4">Neighborhood</TypographyStyled>
+              </GridItem>
+            </GridStyledInnerContainer>
+          </GridFlexed>
+        </>
+      )}
     </GridStyled>
   );
 };
 
-export default SingleViewParcelProprties;
+export default SingleViewParcelProperties;
--- src/modules/land-works/components/land-parcel-properties/styled.tsx
+++ src/modules/land-works/components/land-parcel-properties/styled.tsx
@@ -6,33 +6,41 @@ import { Typography } from 'design-system';
 export const TypographyStyled = styled(Typography)<TypographyProps>(() => ({
   display: 'block',
   textAlign: 'left',
-  marginTop: '10px',
-
   '&.MuiTypography-h1': {
+    textTransform: 'uppercase',
+    fontSize: '20px',
+    fontWeight: '700',
+    lineHeight: '30px',
+    color: 'var(--theme-light-color)',
+  },
+  '&.MuiTypography-h2': {
     textTransform: 'uppercase',
     fontSize: '14px',
     fontWeight: '700',
     lineHeight: '21px',
     color: 'var(--theme-light-color)',
   },
-  '&.MuiTypography-h2': {
-    fontSize: '14px',
-    fontWeight: '500',
-    lineHeight: '21px',
-    color: 'var(--theme-subtle-color)',
-    //minWidth: '150px',
-  },
   '&.MuiTypography-h3': {
     fontSize: '14px',
     fontWeight: '500',
     lineHeight: '21px',
+    color: 'var(--theme-subtle-color)',
+    width: '200px',
+    marginTop: '10px',
+  },
+  '&.MuiTypography-h4': {
+    width: '100%',
+    fontSize: '14px',
+    fontWeight: '500',
+    lineHeight: '21px',
+    marginTop: '10px',
     color: 'var(--theme-light-color)',
   },
 }));
 
 export const GridStyled = styled(Grid)(() => ({
   display: 'flex',
-  flexDirection: 'row',
+  flexDirection: 'column',
   justifyContent: 'space-between',
   background: 'var(--theme-card-color)',
   border: '1px solid rgba(0, 0, 0, 0.1)',
@@ -43,11 +51,23 @@ export const GridStyled = styled(Grid)(() => ({
   margin: '40px 0',
 }));
 
+export const GridItem = styled(Grid)(() => ({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+}));
+
+export const GridFlexed = styled(Grid)(() => ({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+}));
+
 export const GridStyledInnerContainer = styled(Grid)(() => ({
   background: 'var(--theme-modal-color)',
   border: '1px solid rgba(0, 0, 0, 0.1)',
-  //boxShadow: '0px 10px 36px rgba(136, 120, 172, 0.14)',
   boxSizing: 'border-box',
   borderRadius: '20px',
   padding: '20px',
+  height: '185px',
 }));
