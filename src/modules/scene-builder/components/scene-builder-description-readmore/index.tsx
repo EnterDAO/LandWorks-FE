43d71ddf38e990d4a51673dc6313cc87378e8cdd
Anzette Klopper <50806665+zetske@users.noreply.github.com>
2022-05-11 14:49:58 +0200

    Feat/ak/589 sticky components scene builders (#607)
    * set position to fixed & get container height
    
    * add readmore component
    
    * set breadcrumbs component and details container as sticky
    
    * improve sticky styling
    
    * clean up styling
    
    * remove unused hook function
    
    * merge develop into branch
    
    * reduce sliced string in read more function

---
+++ src/modules/scene-builder/components/scene-builder-description-readmore/index.tsx
@@ -0,0 +1,28 @@
+import { FC, useState } from 'react';
+import { styled } from '@mui/system';
+
+interface Props {
+  children: string;
+}
+
+const StyledSpan = styled('span')({
+  color: 'var(--theme-light-color)',
+  cursor: 'pointer',
+  textDecoration: 'underline',
+});
+
+export const ReadMore: FC<Props> = ({ children }) => {
+  const text = children;
+  const [isReadMore, setIsReadMore] = useState(true);
+  const toggleReadMore = () => {
+    setIsReadMore(!isReadMore);
+  };
+  return (
+    <p>
+      {isReadMore ? text.slice(0, 180) : text}
+      <StyledSpan onClick={toggleReadMore} className="read-or-hide">
+        {isReadMore ? '...read more' : ' show less'}
+      </StyledSpan>
+    </p>
+  );
+};
