445cf2b75b314b89bd059358fa93a45e02a01928
Anzette Klopper <50806665+zetske@users.noreply.github.com>
2022-05-11 14:12:43 +0200

    Listing refactoring (#595)
    * create generic contract provider
    
    * test approve function
    
    * refactor useEffect
    
    * clean up & rename provider
    
    * refactor approve function for listing new property
    
    * clean up
    
    * refactor selectedProperty
    
    * clean up
    
    * set variable for selected property metaverse
    
    * set landworks contract and fetch metaverses of component load

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -7,13 +7,11 @@ import ErrorBoundary from 'components/custom/error-boundary';
 import { Loader } from 'design-system';
 import LayoutFooter from 'layout/components/layout-footer';
 import LayoutHeader from 'layout/components/layout-header';
-import EstateRegistryProvider from 'modules/land-works/providers/decentraland/estate-registry-provider';
-import LandRegistryProvider from 'modules/land-works/providers/decentraland/land-registry-provider';
+import ContractProvider from 'modules/land-works/providers/contract-provider';
 import LandWorksProvider from 'modules/land-works/providers/landworks-provider';
 import WarningProvider from 'providers/warning-provider';
 
 import NotionProvider from '../api/notion/client';
-import CryptoVoxelsProvider from '../modules/land-works/providers/cryptovoxels-provider';
 import Erc20Provider from '../modules/land-works/providers/erc20-provider';
 import { GraphClient } from '../web3/graph/client';
 
@@ -35,43 +33,39 @@ const LayoutView: React.FC = () => {
       <div style={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
         <WarningProvider>
           <LandWorksProvider>
-            <LandRegistryProvider>
-              <EstateRegistryProvider>
-                <CryptoVoxelsProvider>
-                  <Erc20Provider>
-                    <ApolloProvider client={client}>
-                      <NotionProvider>
-                        <LayoutHeader />
-                        <main className={classes.main}>
-                          <ErrorBoundary>
-                            <Suspense
-                              fallback={
-                                <Loader
-                                  sx={{
-                                    position: 'fixed',
-                                    top: '50%',
-                                    left: '50%',
-                                    zoom: '0.5',
-                                    transform: 'translate(-50%, -50%)',
-                                  }}
-                                />
-                              }
-                            >
-                              <Switch>
-                                <Route path="/" exact component={LandingView} />
-                                <Route path="/scene-builder" component={SceneBuilderView} />
-                                <Route component={LandworksView} />
-                              </Switch>
-                            </Suspense>
-                          </ErrorBoundary>
-                        </main>
-                        {isntExploreViewRoute && <LayoutFooter />}
-                      </NotionProvider>
-                    </ApolloProvider>
-                  </Erc20Provider>
-                </CryptoVoxelsProvider>
-              </EstateRegistryProvider>
-            </LandRegistryProvider>
+            <ContractProvider>
+              <Erc20Provider>
+                <ApolloProvider client={client}>
+                  <NotionProvider>
+                    <LayoutHeader />
+                    <main className={classes.main}>
+                      <ErrorBoundary>
+                        <Suspense
+                          fallback={
+                            <Loader
+                              sx={{
+                                position: 'fixed',
+                                top: '50%',
+                                left: '50%',
+                                zoom: '0.5',
+                                transform: 'translate(-50%, -50%)',
+                              }}
+                            />
+                          }
+                        >
+                          <Switch>
+                            <Route path="/" exact component={LandingView} />
+                            <Route path="/scene-builder" component={SceneBuilderView} />
+                            <Route component={LandworksView} />
+                          </Switch>
+                        </Suspense>
+                      </ErrorBoundary>
+                    </main>
+                    {isntExploreViewRoute && <LayoutFooter />}
+                  </NotionProvider>
+                </ApolloProvider>
+              </Erc20Provider>
+            </ContractProvider>
           </LandWorksProvider>
         </WarningProvider>
       </div>
---
+++ src/modules/land-works/providers/contract-provider/index.tsx
@@ -0,0 +1,109 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import React, { FC, createContext, useContext, useEffect, useMemo, useState } from 'react';
+import ContractListener from 'web3/components/contract-listener';
+import Web3Contract from 'web3/web3Contract';
+
+import config from 'config';
+import { useReload } from 'hooks/useReload';
+import { useWallet } from 'wallets/wallet';
+
+import CryptoVoxelsContract from '../../contracts/cryptovoxels/CryptoVoxelsContract';
+import EstateRegistryContract from '../../contracts/decentraland/estate/EstateRegistryContract';
+import LANDRegistryContract from '../../contracts/decentraland/land/LANDRegistryContract';
+
+export type ContextType = {
+  cryptoVoxelsContract?: CryptoVoxelsContract;
+  estateRegistryContract?: EstateRegistryContract;
+  landRegistryContract?: LANDRegistryContract;
+  setTxInProgress: React.Dispatch<React.SetStateAction<boolean>>;
+  txInProgress: boolean;
+  txHash: string;
+  setTxHash: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const ProviderContext = createContext<ContextType>({
+  cryptoVoxelsContract: undefined,
+  estateRegistryContract: undefined,
+  landRegistryContract: undefined,
+  txInProgress: false,
+  setTxInProgress: () => {},
+  txHash: '',
+  setTxHash: () => {},
+});
+
+export function useContractRegistry(): ContextType {
+  return useContext(ProviderContext);
+}
+
+const ContractProvider: FC = (props) => {
+  const { children } = props;
+  const [txInProgress, setTxInProgress] = useState(false);
+  const [txHash, setTxHash] = useState('');
+  const walletCtx = useWallet();
+  const [reload] = useReload();
+
+  const landRegistryContract = useMemo(() => {
+    const landRegistry = new LANDRegistryContract([], config.contracts.decentraland.landRegistry);
+    landRegistry.on(Web3Contract.UPDATE_DATA, reload);
+    return landRegistry;
+  }, []);
+
+  const estateRegistryContract = useMemo(() => {
+    const estateRegistry = new EstateRegistryContract([], config.contracts.decentraland.estateRegistry);
+    estateRegistry.on(Web3Contract.UPDATE_DATA, reload);
+    return estateRegistry;
+  }, []);
+
+  const cryptoVoxelsContract = useMemo(() => {
+    const cryptoVoxels = new CryptoVoxelsContract([], config.contracts.cryptoVoxelsContract);
+    cryptoVoxels.on(Web3Contract.UPDATE_DATA, reload);
+    return cryptoVoxels;
+  }, []);
+
+  useEffect(() => {
+    // set providers
+    landRegistryContract.setProvider(walletCtx.provider);
+    estateRegistryContract.setProvider(walletCtx.provider);
+    cryptoVoxelsContract.setProvider(walletCtx.provider);
+  }, [walletCtx.provider]);
+
+  useEffect(() => {
+    // set accounts
+    landRegistryContract.setAccount(walletCtx.account);
+    estateRegistryContract.setAccount(walletCtx.account);
+    cryptoVoxelsContract.setAccount(walletCtx.account);
+  }, [walletCtx.account]);
+
+  const value: ContextType = {
+    landRegistryContract,
+    estateRegistryContract,
+    cryptoVoxelsContract,
+    txInProgress,
+    setTxInProgress,
+    txHash,
+    setTxHash,
+  };
+
+  return (
+    <ProviderContext.Provider value={value}>
+      {children}
+      <ContractListener
+        contract={cryptoVoxelsContract}
+        setLandworksTxInProgress={setTxInProgress}
+        setTxHash={setTxHash}
+      />
+      <ContractListener
+        contract={landRegistryContract}
+        setLandworksTxInProgress={setTxInProgress}
+        setTxHash={setTxHash}
+      />
+      <ContractListener
+        contract={estateRegistryContract}
+        setLandworksTxInProgress={setTxInProgress}
+        setTxHash={setTxHash}
+      />
+    </ProviderContext.Provider>
+  );
+};
+
+export default ContractProvider;
