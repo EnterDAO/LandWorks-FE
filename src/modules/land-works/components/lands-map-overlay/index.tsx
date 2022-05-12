44a4820c976f3e014f0a7affbe39db629356d52f
anzette <anzette@linumlabs.com>
2022-05-12 13:00:56 +0200

    Add hover feature for lands in estates

--- src/modules/land-works/components/lands-map-overlay/index.tsx
+++ src/modules/land-works/components/lands-map-overlay/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { useState } from 'react';
 import { Box } from '@mui/material';
 
 import { ChipStyled, RootStyled, TypographyStyled } from './styled';
@@ -15,17 +17,35 @@ interface IEstateLandOverlay {
 }
 
 const LandsMapOverlay: React.FC<IEstateLandOverlay> = ({ title, coordinates, place }) => {
+  const [expand, setExpand] = useState(false);
+  const coordsLength = coordinates?.length;
+  const hasMoreThanSix = coordsLength! > 6;
+  const hasLessThanSix = coordsLength! > 0 && coordsLength! <= 6;
+
+  const maxWidth = expand ? '22rem' : '15rem';
+
+  const coords = hasMoreThanSix && expand ? coordinates : coordinates?.slice(0, 5);
   return (
-    <RootStyled>
+    <RootStyled style={{ maxWidth: maxWidth }} onMouseLeave={() => setExpand(false)}>
       <TypographyStyled variant="h3">{title}</TypographyStyled>
 
-      {coordinates && coordinates?.length >= 1 && (
+      {coordinates && hasLessThanSix && (
         <div>
           {coordinates.map((estateCoord: Coordinate) => (
             <ChipStyled key={estateCoord.id} label={`X: ${estateCoord.x}, Y: ${estateCoord.y}`} variant="outlined" />
           ))}
         </div>
       )}
+
+      {coordinates && hasMoreThanSix && (
+        <>
+          {coords?.map((estateCoord: Coordinate) => (
+            <ChipStyled key={estateCoord.id} label={`X: ${estateCoord.x}, Y: ${estateCoord.y}`} variant="outlined" />
+          ))}
+          <div onMouseEnter={() => setExpand(true)}>{!expand && <ChipStyled label="More..." variant="outlined" />}</div>
+        </>
+      )}
+
       <>
         {place && place.length && (
           <Box sx={{ display: 'flex', flexDirection: 'column' }}>
