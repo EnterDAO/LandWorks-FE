3d13a50d1f4f698471f12515e891910a8c3f8f31
mishaadzh <work.adzhoiev@gmail.com>
2022-05-30 10:37:11 +0300

    [fix]: land availability

--- src/wallets/components/notifications/utils.ts
+++ src/wallets/components/notifications/utils.ts
@@ -41,7 +41,7 @@ export const parseNotifications = async (
             time: rentStart,
             type: 'newRenting',
             landId: item.id,
-            isAvailable: item?.isAvailable,
+            isAvailable: item.isAvailable || item.availability.isCurrentlyAvailable,
           });
         }
         if (rentEnd <= Date.now()) {
@@ -51,7 +51,7 @@ export const parseNotifications = async (
             time: rentEnd,
             type: 'rentEnded',
             landId: item.id,
-            isAvailable: item?.isAvailable,
+            isAvailable: item.isAvailable || item.availability.isCurrentlyAvailable,
           });
         }
       } else {
@@ -64,7 +64,7 @@ export const parseNotifications = async (
             time: toTimestamp(halfTime),
             type: 'endSoon',
             landId: item.id,
-            isAvailable: item?.isAvailable,
+            isAvailable: item.isAvailable || item.availability.isCurrentlyAvailable,
           });
         }
         if (rentEnd <= Date.now() && isYourRent) {
@@ -74,7 +74,7 @@ export const parseNotifications = async (
             time: rentEnd,
             type: 'yourRentEnded',
             landId: item.id,
-            isAvailable: item?.isAvailable,
+            isAvailable: item.isAvailable || item.availability.isCurrentlyAvailable,
           });
         }
       }
