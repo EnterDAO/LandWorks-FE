ed7717914220479ff17bc3c4adc5138f0ef27ceb
mishaadzh <work.adzhoiev@gmail.com>
2022-06-03 16:32:46 +0300

    typo fix

--- src/modules/landing/index.tsx
+++ src/modules/landing/index.tsx
@@ -1,9 +1,9 @@
-import { About } from './components/About';
+import { About } from './components/about';
 import { Advantages } from './components/Advantages';
 import { Connect } from './components/Connect';
 import { Explore } from './components/Explore';
 import { FAQ } from './components/FAQ';
-import { Hero } from './components/Hero';
+import { Hero } from './components/hero';
 
 const LandingView: React.FC = () => (
   <div className="landing-wrapper">
