fb3e5c4cb92f37a1ecb0e70e89adfced6891f00c
n0vax <agent.aganov@centralsoftwarebureau.com>
2022-08-03 10:42:18 +0300

    Update grands program hero section

--- src/modules/grants-program/components/hero/index.tsx
+++ src/modules/grants-program/components/hero/index.tsx
@@ -1,35 +1,50 @@
 import React from 'react';
 
-import HeroImage from 'assets/img/grandProgramHero.png';
-import { Button } from 'design-system';
+import heroImage from 'assets/img/grandProgramHero.png';
+import { Box, Button, Grid, Typography } from 'design-system';
 
-import { Description, Image, LeftSide, StyledRoot } from './styled';
+import { GRADIENT_TEXT, THEME_COLORS } from 'themes/theme-constants';
 
-export const Hero: React.FC = () => {
+export const Hero = () => {
   return (
-    <StyledRoot>
-      <LeftSide>
-        <h1>Open Scenes</h1>
-        <h2>Grant Program</h2>
-        <Description>
-          <span>
+    <Grid pb={{ xs: 12, lg: 32 }} container display="flex">
+      <Grid display="flex" alignItems="center" justifyContent="center" item xs={12} lg={6}>
+        <Box component="img" maxWidth={540} width={1} src={heroImage} />
+      </Grid>
+      <Grid item xs={12} lg={6} order={{ lg: -1 }}>
+        <Box maxWidth={540}>
+          <Typography
+            mb={{ xs: 3, lg: 8 }}
+            variant="h1"
+            sx={{ fontSize: { xs: '25px', md: '60px' }, lineHeight: { xs: '38px', md: '70px' } }}
+          >
+            Open Scenes
+            <br />
+            <Typography variant="inherit" component="span" sx={GRADIENT_TEXT}>
+              Grant Program
+            </Typography>
+          </Typography>
+
+          <Typography mb={{ xs: 8, md: 12 }} fontSize={{ xs: 16, md: 'auto' }} component="p" variant="subtitle1">
             Bringing creators to the metaverse economy is a fundamental piece to how Landworks and EnterDAO will grow.
             As we empower more artists, we will in turn grow as well and increase our reach and potential to make
             further impact.
-          </span>
-          <p>That’s why we are introducing the first iteration of LandWork’s Open Scene Grant Program.</p>
-        </Description>
-        <Button
-          onClick={() => window.open('https://d1zs47v7suw.typeform.com/to/hs05sYCZ')}
-          variant="gradient"
-          btnSize="medium"
-        >
-          Apply now
-        </Button>
-      </LeftSide>
-      <div>
-        <Image src={HeroImage} />
-      </div>
-    </StyledRoot>
+            <br />
+            <br />
+            <Typography fontWeight={700} color={THEME_COLORS.light} variant="inherit" component="span">
+              That’s why we are introducing the first iteration of LandWork’s Open Scene Grant Program.
+            </Typography>
+          </Typography>
+
+          <Button
+            onClick={() => window.open('https://d1zs47v7suw.typeform.com/to/hs05sYCZ')}
+            variant="gradient"
+            btnSize="medium"
+          >
+            Apply now
+          </Button>
+        </Box>
+      </Grid>
+    </Grid>
   );
 };
