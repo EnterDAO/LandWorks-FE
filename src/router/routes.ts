4069c17b937c51d3fcd259b388766598637ba94e
n0vax <agent.aganov@centralsoftwarebureau.com>
2022-08-04 23:23:29 +0300

    Update routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,36 +1,21 @@
-import { generatePath, useRouteMatch } from 'react-router-dom';
+import { generatePath } from 'react-router-dom';
 
-export const mainRoutes = {
+export const routes = {
   home: '/',
-  faq: '/faq',
   docs: '/docs',
+  faq: '/faq',
   sceneBuilders: '/scene-builder',
   sceneBuildersJoin: '/scene-builder/join',
   sceneBuildersBuilder: '/scene-builder/builder/:builderName',
   grantsProgram: '/grants-program',
+
+  explore: '/explore',
+  myProperties: '/my-properties',
+  property: '/property/:tokenId',
 };
 
-export const appRoutes = {
-  explore: '/app',
-  myProperties: '/app/my-properties',
-  property: '/app/property/:tokenId',
-  faq: '/app/faq',
-  sceneBuilders: '/app/scene-builder',
-  sceneBuildersJoin: '/app/scene-builder/join',
-  sceneBuildersBuilder: '/app/scene-builder/builder/:builderName',
-  grantsProgram: '/app/grants-program',
-};
-
-export const useIsAppRoute = () => {
-  return !!useRouteMatch({
-    path: appRoutes.explore,
+export const getSceneBuilderBuilderPath = (builderName: string) => {
+  return generatePath(routes.sceneBuildersBuilder, {
+    builderName,
   });
-};
-
-// TODO: find better way for handling sub routes
-export const usePath = (route: string, params?: Record<string, string>) => {
-  const isAppRoute = useIsAppRoute();
-  const path = isAppRoute && !route.startsWith(appRoutes.explore) ? `${appRoutes.explore}${route}` : route;
-
-  return generatePath(path, params);
 };
