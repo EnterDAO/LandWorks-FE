b9e008c72b53b748146a0e5a19996e3fe14aee62
n0vax <agent.aganov@centralsoftwarebureau.com>
2022-08-12 19:55:02 +0300

    Update LandWorksCardExploreView component

--- src/modules/land-works/components/land-works-card-explore-view/index.tsx
+++ src/modules/land-works/components/land-works-card-explore-view/index.tsx
@@ -1,4 +1,5 @@
 import React, { SyntheticEvent, useState } from 'react';
+import classNames from 'classnames';
 import { ZERO_BIG_NUMBER } from 'web3/utils';
 
 import Icon from 'components/custom/icon';
@@ -10,15 +11,18 @@ import { AssetEntity } from '../../api';
 import LandCardAvailability from '../land-works-card-availability';
 import { ReactComponent as HotIcon } from './assets/hot.svg';
 
+import { shortenString } from 'modules/land-works/utils';
+
 import './index.scss';
 
 interface Props {
   land: AssetEntity;
+  layout?: 'normal' | 'compact';
   onClick?: (e: SyntheticEvent, land: AssetEntity) => void;
   onMouseOver?: (e: SyntheticEvent, land: AssetEntity) => void;
 }
 
-const LandWorksCard: React.FC<Props> = ({ land, onClick, onMouseOver }) => {
+const LandWorksCard: React.FC<Props> = ({ land, onClick, onMouseOver, layout = 'normal' }) => {
   const { clickedLandId } = useLandsMapTile();
   const did = land.decentralandData
     ? `${land.decentralandData?.coordinates[0]?.x},${land.decentralandData?.coordinates[0]?.y}`
@@ -40,9 +44,11 @@ const LandWorksCard: React.FC<Props> = ({ land, onClick, onMouseOver }) => {
     }
   };
 
+  const ownerWalletAddress = land.decentralandData?.asset.owner.id;
+
   return (
     <a
-      className={`land-explore-card${isActive ? ' active' : ''}`}
+      className={classNames('land-explore-card', `land-explore-card--layout-${layout}`, isActive && 'active')}
       onClick={(e) => {
         e.preventDefault();
         !!onClick && onClick(e, land);
@@ -52,7 +58,7 @@ const LandWorksCard: React.FC<Props> = ({ land, onClick, onMouseOver }) => {
       id={`land-explore-card--${did}`}
       href={`/property/${land.id}`}
     >
-      <div style={{ pointerEvents: 'none' }}>
+      <div>
         <div className="land-explore-image">
           <img className="land-explore-image-img" src={land.imageUrl} alt="land-explore-image-img" />
         </div>
@@ -62,48 +68,55 @@ const LandWorksCard: React.FC<Props> = ({ land, onClick, onMouseOver }) => {
           </span>
         )}
 
-        <div className="land-explore-name">
-          <span title={land.name}>{land.name.toLowerCase()}</span>
+        <div className="land-explore-card__info">
+          <div className="land-explore-card__name-and-address">
+            <p className="land-explore-card__name" title={land.name}>
+              {land.name.toLowerCase()}
+            </p>
+            {!!ownerWalletAddress && (
+              <p className="land-explore-card__address" title={ownerWalletAddress}>
+                BY {shortenString(ownerWalletAddress)}
+              </p>
+            )}
+          </div>
+
+          <div className="land-explore-card__price">
+            <span className="land-explore-price-crypto">
+              <SmallAmountTooltip
+                className="land-explore-price-amount"
+                amount={land.pricePerMagnitude.price}
+                icon={
+                  <Icon
+                    name={getTokenIconName(land.paymentToken.symbol)}
+                    className="land-explore-price-crypto-icon eth-icon"
+                  />
+                }
+              />
+            </span>
+
+            <span className="land-explore-price">
+              <SmallAmountTooltip symbol="$" amount={land.pricePerMagnitude.usdPrice || ZERO_BIG_NUMBER} />
+              <span>/{land.pricePerMagnitude.magnitude}</span>
+            </span>
+          </div>
         </div>
 
-        <div className="land-explore-row start">
-          <span className="land-explore-price-crypto">
-            <SmallAmountTooltip
-              className="land-explore-price-amount"
-              amount={land.pricePerMagnitude.price}
-              icon={
-                <Icon
-                  name={getTokenIconName(land.paymentToken.symbol)}
-                  className="land-explore-price-crypto-icon eth-icon"
-                />
-              }
-            />
-          </span>
+        <div className="land-explore-divider" />
 
-          <span className="land-explore-price">
-            <SmallAmountTooltip symbol="$" amount={land.pricePerMagnitude.usdPrice || ZERO_BIG_NUMBER} />
-            <span>/{land.pricePerMagnitude.magnitude}</span>
-          </span>
-        </div>
-
-        <div className="land-explore-divider"></div>
-
-        <div className="land-explore-row">
-          <div>
+        <div className="land-explore-card__rent land-explore-row">
+          <div className="land-explore-card__rent-period">
             <span className="land-explore-rent-label">Rent period</span>
             <p className="land-explore-rent-value">
               {land.minPeriodTimedType}-{land.maxPeriodTimedType}
             </p>
           </div>
-          <div>
-            <LandCardAvailability land={land} />
+          <div className="land-explore-card__rent-status">
+            <LandCardAvailability layout={layout} land={land} />
           </div>
         </div>
 
-        <div className="land-explore-row start">
-          <div className="land-explore-hashtags">
-            #{land.type} #{land.metaverse.name}
-          </div>
+        <div className="land-explore-card__hashtags land-explore-row">
+          #{land.type} #{land.metaverse.name}
         </div>
       </div>
     </a>
