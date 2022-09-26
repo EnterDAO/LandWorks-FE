ab937b309accb5997d7fd2f7c944eae49eed246e
n0vax <agent.aganov@centralsoftwarebureau.com>
2022-09-26 09:26:51 +0300

    Update faq section on home page

--- src/modules/landing/components/FAQ/index.tsx
+++ src/modules/landing/components/FAQ/index.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
+import { Box, Button, Stack, Typography } from 'design-system';
 import { routes } from 'router/routes';
 
 import { questionData } from './data';
 import { QuestionItem } from './QuestionItem';
-import { QuestionList, StyledLink, StyledRoot, StyledTypography } from './styled';
+
+import { GRADIENT_TEXT } from 'themes/theme-constants';
 
 export const FAQ: React.FC = () => {
+  const history = useHistory();
+
   return (
-    <div className="content-container">
-      <StyledRoot>
-        <StyledTypography>
-          Frequently Asked <span>Questions</span>
-        </StyledTypography>
-        <QuestionList>
-          {questionData.map((item) => (
-            <QuestionItem item={item} key={item.id} />
-          ))}
-        </QuestionList>
-        <StyledLink to={routes.faq}>See all FAQ</StyledLink>
-      </StyledRoot>
-    </div>
+    <Stack alignItems="center" py={{ xs: 10, lg: 20 }} className="content-container">
+      <Typography textAlign="center" variant="h1" component="h2" mb={{ xs: 10, lg: 12 }}>
+        Frequently Asked
+        <br />
+        <Typography variant="inherit" sx={GRADIENT_TEXT}>
+          Questions
+        </Typography>
+      </Typography>
+      <Box maxWidth={680} mb={10}>
+        {questionData.map((item) => (
+          <QuestionItem item={item} key={item.id} />
+        ))}
+      </Box>
+      <Button onClick={() => history.push(routes.faq)} variant="primary" btnSize="small">
+        See all FAQ
+      </Button>
+    </Stack>
   );
 };
