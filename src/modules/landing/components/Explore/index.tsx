69389a0f5c79867f225925a10249b37138dd65cd
n0vax <agent.aganov@centralsoftwarebureau.com>
2022-09-26 09:26:43 +0300

    Update explore section on home page

--- src/modules/landing/components/Explore/index.tsx
+++ src/modules/landing/components/Explore/index.tsx
@@ -3,27 +3,45 @@ import { useHistory } from 'react-router-dom';
 
 import { ReactComponent as Tag } from 'assets/img/explore_tag.svg';
 import { ReactComponent as LandGroup } from 'assets/img/land_group.svg';
-import { Button } from 'design-system';
+import { Box, Button, Grid, Typography } from 'design-system';
 import { StyledRoot } from 'modules/landing/components/Explore/styled';
 import { routes } from 'router/routes';
 
 export const Explore: React.FC = () => {
   const history = useHistory();
   return (
-    <div className="content-container">
-      <StyledRoot>
-        <div>
-          <Tag />
-          <h1>
+    <Box py={{ xs: 10, lg: 20 }} className="content-container">
+      <Box
+        display="flex"
+        flexDirection={{ xs: 'column', lg: 'row' }}
+        justifyContent="space-between"
+        alignItems="center"
+        border="5px solid #27273A"
+        bgcolor="#1E1E2E"
+        borderRadius="50px"
+      >
+        <Box
+          order={{ xs: 1, lg: 0 }}
+          p={{ xs: '0 20px 40px 20px', lg: '70px', xl: '120px 0px 0px 140px' }}
+          flexShrink={0}
+        >
+          <Box mb={2}>
+            <Tag />
+          </Box>
+          <Typography variant="h1" component="h2" mb={2}>
             Explore <br /> Our Marketplace
-          </h1>
-          <p>Rent a land or list your property for renting - we make it easy for you.</p>
-          <Button onClick={() => history.push(routes.explore)} btnSize="small" variant="gradient">
+          </Typography>
+          <Typography maxWidth={300} mb={9}>
+            Rent a land or list your property for renting - we make it easy for you.
+          </Typography>
+          <Button onClick={() => history.push(routes.explore)} btnSize="medium" variant="gradient">
             Explore
           </Button>
-        </div>
-        <LandGroup />
-      </StyledRoot>
-    </div>
+        </Box>
+        <Box maxWidth={600} order={{ xs: 0, lg: 1 }} sx={{ svg: { width: 1, height: 'auto' } }}>
+          <LandGroup />
+        </Box>
+      </Box>
+    </Box>
   );
 };
