bb252e6eefff2b49da5242e895bad4f5524e5022
failfmi <oscurocalma@gmail.com>
2022-10-01 23:52:04 +0300

    feat: referrals (#877)

--- src/modules/land-works/contracts/core/LandWorksContract.ts
+++ src/modules/land-works/contracts/core/LandWorksContract.ts
@@ -7,6 +7,7 @@ import Web3Contract, { Web3ContractAbiItem } from 'web3/web3Contract';
 import LandWorksABI from './abi.json';
 
 import { isDecentralandMetaverseRegistry } from '../../../../utils';
+import { DEFAULT_ADDRESS } from '../../../../web3/utils';
 
 export default class LandWorksContract extends Web3Contract {
   constructor(abi: AbiItem[], address: string) {
@@ -29,6 +30,7 @@ export default class LandWorksContract extends Web3Contract {
    * @param maxFutureTime
    * @param paymentToken
    * @param pricePerSecond
+   * @param callback Function, executed once the transaction has been submitted
    */
   list(
     metaverseId: BigNumber | number,
@@ -46,7 +48,7 @@ export default class LandWorksContract extends Web3Contract {
     }
 
     return this.send(
-      'list',
+      this.listMethod(metaverseRegistry, true),
       [
         metaverseId,
         metaverseRegistry,
@@ -56,6 +58,7 @@ export default class LandWorksContract extends Web3Contract {
         maxFutureTime,
         paymentToken,
         pricePerSecond,
+        DEFAULT_ADDRESS,
       ],
       {
         from: this.account,
@@ -72,6 +75,7 @@ export default class LandWorksContract extends Web3Contract {
    * @param maxFutureTime
    * @param paymentToken The payment token for future rents.
    * @param pricePerSecond The price per second in the lowest denomination.
+   * @param callback Function, executed once the transaction has been submitted
    */
   updateConditions(
     assetId: BigNumber | string,
@@ -99,6 +103,7 @@ export default class LandWorksContract extends Web3Contract {
   /**
    * Delists the asset.
    * @param assetId The target asset.
+   * @param callback Function, executed once the transaction has been submitted
    */
   delist(assetId: BigNumber | string, callback: () => void = () => {}): Promise<void> {
     if (!this.account) {
@@ -117,6 +122,7 @@ export default class LandWorksContract extends Web3Contract {
   /**
    * Withdraws the asset.
    * @param assetId The target asset.
+   * @param callback Function, executed once the transaction has been submitted
    */
   withdraw(assetId: BigNumber | string, callback: () => void = () => {}): Promise<void> {
     if (!this.account) {
@@ -135,6 +141,7 @@ export default class LandWorksContract extends Web3Contract {
   /**
    * Claims the rent fees for the given assets.
    * @param assetIds An array of asset IDs
+   * @param callback Function, executed once the transaction has been submitted
    */
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   claimMultipleRentFees(assetIds: BigNumber[] | string[], callback: () => void = () => {}) {
@@ -155,9 +162,13 @@ export default class LandWorksContract extends Web3Contract {
   /**
    * Rents an asset, which has ETH as fee payment. Calculates the total value based on period * pricePerSecond of the asset.
    * @param assetId The given asset.
+   * @param metaverseRegistry The address of the Metaverse Registry
    * @param operator The operator to be set.
    * @param period The period (in seconds) for the rent.
+   * @param maxRentStart The accepted max rent start (in seconds)
+   * @param paymentToken The address of the payment token
    * @param value The value to be sent.
+   * @param callback Function, executed once the transaction has been submitted
    */
   rentWithETH(
     assetId: BigNumber | string,
@@ -175,7 +186,7 @@ export default class LandWorksContract extends Web3Contract {
 
     return this.send(
       this.rentMethod(metaverseRegistry),
-      [assetId, period.toNumber(), maxRentStart, operator, paymentToken, value],
+      [assetId, period.toNumber(), maxRentStart, operator, paymentToken, value, DEFAULT_ADDRESS],
       {
         from: this.account,
         value: value,
@@ -187,8 +198,13 @@ export default class LandWorksContract extends Web3Contract {
   /**
    * Rents an asset, which has an ERC-20 as fee payment.
    * @param assetId The given asset.
+   * @param metaverseRegistry The address of the Metaverse Registry
    * @param operator The operator to be set.
    * @param period The period (in seconds) for the rent.
+   * @param maxRentStart The accepted max rent start (in seconds)
+   * @param paymentToken The address of the payment token
+   * @param value The value to be sent.
+   * @param callback Function, executed once the transaction has been submitted
    */
   rentWithERC20(
     assetId: BigNumber | string,
@@ -205,7 +221,7 @@ export default class LandWorksContract extends Web3Contract {
     }
     return this.send(
       this.rentMethod(metaverseRegistry),
-      [assetId, period, maxRentStart, operator, paymentToken, value],
+      [assetId, period, maxRentStart, operator, paymentToken, value, DEFAULT_ADDRESS],
       {
         from: this.account,
       },
@@ -216,9 +232,10 @@ export default class LandWorksContract extends Web3Contract {
   /**
    * Update the operator for the given rent of an asset.
    * @param assetId The target asset.
-   * @param metaverseRegistry The target metaverseRegistry
+   * @param metaverseRegistry The address of the Metaverse Registry
    * @param rentId The target rent.
    * @param operator The to-be-set operator.
+   * @param callback Function, executed once the transaction has been submitted
    */
   updateOperator(
     assetId: BigNumber | string,
@@ -245,6 +262,7 @@ export default class LandWorksContract extends Web3Contract {
    * @param assetId The target asset id
    * @param metaverseRegistry The asset's metaverse registry
    * @param rentId The target rent id
+   * @param callback Function, executed once the transaction has been submitted
    */
   updateState(
     assetId: BigNumber | string,
@@ -270,7 +288,7 @@ export default class LandWorksContract extends Web3Contract {
    * Reverts if there is an active rent.
    * @param assetId The target asset id
    * @param metaverseRegistry The asset's metaverse registry
-   * @param callback Action to be done once transaction is submitted
+   * @param callback Function, executed once the transaction has been submitted
    */
   updateAdministrativeState(
     assetId: BigNumber | string,
@@ -289,6 +307,63 @@ export default class LandWorksContract extends Web3Contract {
       },
       callback
     ).then();
+  }
+
+  /**
+   * Returns the payment token and payment amount, which needs to be provided upon renting
+   * The calculations are based on the asset id, period and referrer.
+   * Depending on the referrer, the renter might receive a discount on the initial amount (period * pricePerSecond)
+   * Call this whenever the referrer is != AddressZero
+   * @param assetId The target asset id
+   * @param period The period (in seconds) for the rent.
+   * @param referrer The address of the referrer
+   */
+  async calculateRentFee(assetId: BigNumber | string, period: BigNumber, referrer: string): Promise<any> {
+    const [paymentToken, amount] = await this.call('calculateRentFee', [assetId, period, referrer]);
+
+    return {
+      paymentToken,
+      amount,
+    };
+  }
+
+  /**
+   * Updates the assets, which have LANDs with operators, != AddressZero
+   * Applies only for asset's which represent Decentraland Estates
+   * Reverts if there is an active rent.
+   * @param assetIds The list of asset ids
+   * @param landIds The ids of the lands, which are part of the corresponding assetId (estate)
+   * @param callback Function, executed once the transaction has been submitted
+   */
+  clearEstateLANDOperators(
+    assetIds: BigNumber[] | string[],
+    landIds: BigNumber[] | string[],
+    callback: () => void = () => {}
+  ): Promise<void> {
+    if (!this.account) {
+      return Promise.reject();
+    }
+    return this.send(
+      'clearEstateLANDOperators',
+      [assetIds],
+      {
+        from: this.account,
+      },
+      callback
+    ).then();
+  }
+
+  /**
+   * Returns the list method to be called based on metaverse registry and user agreement for promotional scene
+   * @param metaverseRegistry The address of the metaverse registry
+   * @param hasUserAgreedForPromotionalScene Whether the user has agreed for promotional scene
+   */
+  listMethod(metaverseRegistry: string, hasUserAgreedForPromotionalScene: boolean): string {
+    if (isDecentralandMetaverseRegistry(metaverseRegistry)) {
+      return hasUserAgreedForPromotionalScene ? 'listDecentraland' : 'list';
+    } else {
+      return 'list';
+    }
   }
 
   /**
