75aa0f723ccdfbdcd048dfa8bf91c9150def3315
n0vax <agent.aganov@centralsoftwarebureau.com>
2022-11-03 11:07:03 +0300

    feat: add context helpers

---
+++ src/utils/context.ts
@@ -0,0 +1,27 @@
+import { ConsumerProps, Context, createContext, useContext } from 'react';
+
+const defaultValue = undefined;
+type DefaultValue = undefined;
+
+export const createSafeContext = <ContextValue>() => {
+  return createContext<ContextValue | DefaultValue>(defaultValue);
+};
+
+export const useSafeContext = <T>(context: Context<T | DefaultValue>): T => {
+  const value = useContext(context);
+
+  if (value === defaultValue) {
+    throw new Error('no value provided for context');
+  }
+
+  return value as T;
+};
+
+export const createSafeConsumer = <T>(context: Context<T | DefaultValue>) => {
+  const SafeConsumer = ({ children }: ConsumerProps<T>) => {
+    const value = useSafeContext(context);
+    return children(value);
+  };
+
+  return SafeConsumer;
+};
