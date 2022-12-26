b9ea8eb28011d49e58d26ebf72f77a9a92cd44e9
n0vax <agent.aganov@centralsoftwarebureau.com>
2022-12-26 13:31:55 +0300

    fix(faq-page): update faq page

---
+++ src/modules/faq/components/FaqWhyMetamaskAskPermissionToCollection.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const FaqWhyMetamaskAskPermissionToCollection = () => {
+  return (
+    <>
+      <p>
+        The reason is gas fee optimization so you can transact with the protocol for more than one property with a
+        single approval.
+      </p>
+      <p>
+        For example, if you want to list 10 lands for rent, you would need to approve once for access per Metaverse
+        rather than pay gas every time.
+      </p>
+      <p>We have decided this based on the fact that the majority of our landlords list more than 1 land.</p>
+    </>
+  );
+};
+
+export default FaqWhyMetamaskAskPermissionToCollection;
