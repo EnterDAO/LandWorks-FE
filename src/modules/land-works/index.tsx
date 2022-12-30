1f6177aee68e98b339a8d3182a954942023fd0d5
n0vax <agent.aganov@centralsoftwarebureau.com>
2022-12-30 15:49:15 +0300

    feat: add preloading of the metaverses and payment tokens before showing the app pages

--- src/modules/land-works/index.tsx
+++ src/modules/land-works/index.tsx
@@ -1,18 +1,39 @@
 import React, { lazy, useEffect } from 'react';
 import { isMobile } from 'react-device-detect';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
+import { Loader } from 'design-system';
+import useGetAllMetaversesQuery from 'hooks/useGetAllMetaversesQuery';
+import useGetAllPaymentTokensQuery from 'hooks/useGetAllPaymentTokensQuery';
 import ListingModalProvider from 'providers/listing-modal-provider';
 import { useWarning } from 'providers/warning-provider';
-import { APP_ROUTES } from 'router/routes';
+import { APP_ROUTES, LANDING_ROUTES } from 'router/routes';
+
+import { PaymentToken } from './api';
+
+import { createDataProvider } from 'utils/context';
 
 const ExploreView = lazy(() => import('./views/explore-view'));
 const MyPropertiesView = lazy(() => import('./views/my-properties-view'));
 const SingleLand = lazy(() => import('./views/single-land-view'));
 
-const LandworksView: React.FC = () => {
+export const { DataProvider: MetaversesProvider, useData: useMetaverses } =
+  createDataProvider<{ id: string; name: string }[]>();
+
+export const { DataProvider: PaymentTokensProvider, useData: usePaymentTokens } = createDataProvider<PaymentToken[]>();
+
+const LandworksView = () => {
+  const { data: metaverses, loading: areMetaversesLoading, error: metaversesQueryError } = useGetAllMetaversesQuery();
+  const {
+    data: paymentTokens,
+    loading: arePaymentTokensLoading,
+    error: paymentTokensQueryError,
+  } = useGetAllPaymentTokensQuery();
+
   const warning = useWarning();
 
+  const error = metaversesQueryError || paymentTokensQueryError;
+  const isLoading = areMetaversesLoading || arePaymentTokensLoading;
   useEffect(() => {
     if (!isMobile) {
       return;
@@ -25,14 +46,37 @@ const LandworksView: React.FC = () => {
     });
   }, [isMobile]);
 
+  if (isLoading) {
+    return (
+      <Loader
+        sx={{
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          zoom: '0.5',
+          transform: 'translate(-50%, -50%)',
+        }}
+      />
+    );
+  }
+
+  if (error || !metaverses || !paymentTokens) {
+    // TODO: notify user about error or show error page?
+    return <Redirect to={LANDING_ROUTES.home} />;
+  }
+
   return (
-    <ListingModalProvider>
-      <Switch>
-        <Route path={APP_ROUTES.explore} exact component={ExploreView} />
-        <Route path={APP_ROUTES.property} component={SingleLand} />
-        <Route path={APP_ROUTES.myProperties} component={MyPropertiesView} />
-      </Switch>
-    </ListingModalProvider>
+    <MetaversesProvider value={metaverses}>
+      <PaymentTokensProvider value={paymentTokens}>
+        <ListingModalProvider>
+          <Switch>
+            <Route path={APP_ROUTES.explore} exact component={ExploreView} />
+            <Route path={APP_ROUTES.property} component={SingleLand} />
+            <Route path={APP_ROUTES.myProperties} component={MyPropertiesView} />
+          </Switch>
+        </ListingModalProvider>
+      </PaymentTokensProvider>
+    </MetaversesProvider>
   );
 };
 
