eb1787701510cab493c8041d49b6ed70de85ee29
Alexander Aganov <96529778+n0vax@users.noreply.github.com>
2023-01-24 18:12:49 +0300

    [1011, 1024]: Split metacreators in two words; Center cards description; Add discord link; Remove wrapping for read more button  (#1083)
    * fix(faq): add discord link
    
    * fix(metacreator-page): remove wrapping for read more button
    
    * fix(metacreators-page): make description of the metacreator centered
    
    * fix: split metacreators in two words
    
    Co-authored-by: failfmi <oscurocalma@gmail.com>

--- src/modules/faq/components/FaqSupport.tsx
+++ src/modules/faq/components/FaqSupport.tsx
@@ -4,12 +4,15 @@ const FaqSupport = () => {
   return (
     <>
       <p>
-        You are welcomed in our Discord server where we either the LandWorks staff or our amazing community members will
-        help you out.
+        You are welcomed in our{' '}
+        <a href="https://discord.com/invite/7QJvEctG2G" target="_blank" rel="noopener noreferrer referrer">
+          Discord server
+        </a>{' '}
+        where we either the LandWorks staff or our amazing community members will help you out.
       </p>
       <p>
         Or you can simply tweet at us at{' '}
-        <a href="https://twitter.com/landworksxyz" target="_blank">
+        <a href="https://twitter.com/landworksxyz" target="_blank" rel="noopener noreferrer referrer">
           @landworksxyz
         </a>
       </p>
