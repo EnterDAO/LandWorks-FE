9f9c517f579917e805eaa97e0d77804fea56176b
n0vax <agent.aganov@centralsoftwarebureau.com>
2023-01-27 17:00:20 +0300

    fix: add scene and metaverse details

--- src/modules/land-works/components/list-new-property/MarketplaceAssetSummary/MarketplaceAssetSummary.tsx
+++ src/modules/land-works/components/list-new-property/MarketplaceAssetSummary/MarketplaceAssetSummary.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import BigNumber from 'bignumber.js';
 
+import promotionalSceneTooltipThumbnail from 'assets/img/promotional-scene-thumbnail.jpg';
 import Icon from 'components/custom/icon';
 import TokenIcon from 'components/custom/token-icon';
-import { Stack } from 'design-system';
+import { Stack, Tooltip } from 'design-system';
 import { Box, Grid, Typography } from 'design-system';
 import { getTokenIconName } from 'helpers/helpers';
 import { MarketplaceAsset } from 'hooks/useGetAssetsForBuyingQuery';
@@ -11,6 +12,8 @@ import { Option } from 'modules/interface';
 import { PaymentToken } from 'modules/land-works/api';
 
 import { DAY_IN_SECONDS, HOUR_IN_SECONDS, MINUTE_IN_SECONDS, MONTH_IN_SECONDS, WEEK_IN_SECONDS } from 'utils/date';
+
+import { THEME_COLORS } from 'themes/theme-constants';
 
 import s from './s.module.scss';
 
@@ -68,7 +71,7 @@ const MarketplaceAssetSummary = ({
   };
 
   return (
-    <Grid className={s.wrapper} mt={metaverse ? 0 : 4} zeroMinWidth item>
+    <Grid className={s.wrapper} zeroMinWidth item>
       <Grid className={s.card}>
         <Stack textAlign="left" gap="4px">
           <Typography color="var(--theme-light-color)" fontWeight={600}>
@@ -128,19 +131,43 @@ const MarketplaceAssetSummary = ({
                 {rentPrice.toNumber()} {paymentToken.symbol}
               </p>
             </Grid>
-            {metaverse && (
-              <Grid item xs={6} display="flex" flexDirection="column">
-                Metaverse
-                <Box component="p" display="flex" alignItems="center">
-                  {metaverse.icon && (
-                    <Box display="inline-flex" mr={1} sx={{ img: { width: 15, height: 15 } }}>
-                      {metaverse.icon}
-                    </Box>
-                  )}
-                  {metaverse.label}
+            <Grid item xs={6} display="flex" flexDirection="column">
+              Metaverse
+              <Box component="p" display="flex" alignItems="center">
+                <Box display="inline-flex" mr={1} sx={{ img: { width: 15, height: 15 } }}>
+                  <Icon name="png/Decentraland" width="20" height="20" />
                 </Box>
-              </Grid>
-            )}
+                Decentraland
+              </Box>
+            </Grid>
+
+            <Grid item xs={12} display="flex" flexDirection="column">
+              <Typography variant="inherit" component="span" display="inline-flex" alignItems="center" gap="4px">
+                SCENE
+                <Tooltip
+                  arrow
+                  title={
+                    <Box display="flex" gap={2}>
+                      <Box borderRadius="10px" src={promotionalSceneTooltipThumbnail} component="img" width={170} />
+                      <Box>
+                        <Typography mb={1} fontWeight={400} variant="body2" color={THEME_COLORS.darkBlue01}>
+                          Promotional Scene
+                        </Typography>
+                        <Typography fontWeight={400} variant="body2">
+                          A “For Rent” scene with an embedded link will be deployed on the property. The scene boosts
+                          chances for rent.
+                        </Typography>
+                      </Box>
+                    </Box>
+                  }
+                >
+                  <Box display="inline-flex" alignItems="center" justifyContent="center">
+                    <Icon style={{ marginLeft: 0 }} name="about" className="info-icon" />
+                  </Box>
+                </Tooltip>
+              </Typography>
+              <p>LandWorks Promotional Scene</p>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
