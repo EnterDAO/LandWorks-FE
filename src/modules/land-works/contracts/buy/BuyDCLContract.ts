81b2ebde60cead1c37e01c5a815d1485bbbce7a7
failfmi <oscurocalma@gmail.com>
2023-01-30 17:31:00 +0200

    feat(buy-contract): switch interface to contract

--- src/modules/land-works/contracts/buy/BuyDCLContract.ts
+++ src/modules/land-works/contracts/buy/BuyDCLContract.ts
@@ -8,8 +8,6 @@ import { DEFAULT_ADDRESS } from '../../../../web3/utils';
 
 export interface ILandWorksList {
   metaverseId: BigNumber | number;
-  metaverseRegistry: string;
-  metaverseAssetId: string;
   minPeriod: BigNumber | string;
   maxPeriod: BigNumber | string;
   maxFutureTime: BigNumber | string;
@@ -26,25 +24,21 @@ export default class BuyDCLContract extends Web3Contract {
   /**
    * Executes buyETH contract function, which buys and lists an ERC-721 into LandWorks with ERC-20.
    * @param listArgs LandWorks listing arguments.
-   * @param paymentToken The address of the ERC-20 payment
+   * @param seaportParams Seaport fulfillBasicOrder arguments.
    * @param paymentAmount The amount of ERC-20
-   * @param buyContract The address of the buy contract.
-   * @param buyData The data purchase.
    * @param callback Function, executed once the transaction has been submitted
    */
   buyERC20(
     listArgs: ILandWorksList,
-    paymentToken: string,
+    seaportParams: string,
     paymentAmount: string,
-    buyContract: string,
-    buyData: string,
     callback: (txHash: string) => void = () => {}
   ): Promise<any> {
     listArgs.referrer = DEFAULT_ADDRESS;
 
     return this.send(
-      'buyERC20',
-      [listArgs, paymentToken, paymentAmount, buyContract, buyData],
+      'buyList',
+      [listArgs, seaportParams, paymentAmount],
       {
         from: this.account,
       },
@@ -55,23 +49,21 @@ export default class BuyDCLContract extends Web3Contract {
   /**
    * Executes buyETH contract function, which buys and lists an ERC-721 into LandWorks with ETH.
    * @param listArgs LandWorks listing arguments.
+   * @param seaportParams Seaport fulfillBasicOrder arguments.
    * @param paymentAmount Amount to be paid to the buyContract.
-   * @param buyContract The address of the buy contract.
-   * @param buyData The data purchase.
    * @param callback Function, executed once the transaction has been submitted
    */
   buyETH(
     listArgs: ILandWorksList,
+    seaportParams: string,
     paymentAmount: BigNumber | string,
-    buyContract: string,
-    buyData: string,
     callback: (txHash: string) => void = () => {}
   ): Promise<any> {
     listArgs.referrer = DEFAULT_ADDRESS;
 
     return this.send(
-      'buyETH',
-      [listArgs, buyContract, buyData],
+      'buyList',
+      [listArgs, seaportParams, paymentAmount],
       {
         from: this.account,
         value: paymentAmount,
